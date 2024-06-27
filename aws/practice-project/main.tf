e59e53c47ba4b98633a18b0f6b04806a96db2ba8
Sahil Randhawa <98905306+sahil-randhawa@users.noreply.github.com>
2024-06-27 15:27:23 -0400

    main file complete v1

--- aws/practice-project/main.tf
+++ aws/practice-project/main.tf
@@ -43,8 +43,8 @@ resource "aws_route_table" "prod-route-table" {
   }
 
   route {
-    ipv6_cidr_block        = "::/0"
-    egress_only_gateway_id = aws_internet_gateway.gw.id
+    ipv6_cidr_block = "::/0"
+    gateway_id = aws_internet_gateway.gw.id
   }
 
   tags = {
@@ -57,6 +57,7 @@ resource "aws_route_table" "prod-route-table" {
 resource "aws_subnet" "subnet-1" {
   vpc_id = aws_vpc.prod-vpc.id
   cidr_block = "10.0.1.0/24"
+  availability_zone = "us-east-1a"
 
   tags = {
     Name = "prod-subnet"
@@ -120,14 +121,49 @@ resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
   ip_protocol       = "-1" # semantically equivalent to all ports
 }
 
-resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv6" {
-  security_group_id = aws_security_group.allow_tls.id
-  cidr_ipv6         = "::/0"
-  ip_protocol       = "-1" # semantically equivalent to all ports
-}
+#resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv6" {
+#  security_group_id = aws_security_group.allow_tls.id
+#  cidr_ipv6         = "::/0"
+#  ip_protocol       = "-1" # semantically equivalent to all ports
+#}
 
 # # 7. Create a network interface with an ip in the subnet that was created in step 4
 
+resource "aws_network_interface" "web-server-nic" {
+  subnet_id       = aws_subnet.subnet-1.id
+  private_ips     = ["10.0.1.50"]
+  security_groups = [aws_security_group.allow_web.id]
+}
+
 # # 8. Assign an elastic IP to the network interface created in step 7
 
+resource "aws_eip" "one" {
+  domain                    = "vpc"
+  network_interface         = aws_network_interface.web-server-nic.id
+  associate_with_private_ip = "10.0.1.50"
+  depends_on                = [aws_internet_gateway.gw]
+}
+
 # # 9. Create Ubuntu server and install/enable apache2
+
+resource "aws_instance" "web-server-instance"{
+  ami               = "ami-085925f297f89fce1"
+  instance_type     = "t2.micro"
+  availability_zone = "us-east-1a"
+  key_name = "templlc"
+
+  network_interface {
+    device_index = 0
+    network_interface_id = aws_network_interface.web-server-nic.id
+  }
+
+  user_data = <<-EOF
+                #!bin/bash
+                sudo apt update -y
+                sudo systemctl start apache2
+                sudo bash -c 'echo your first web server > /var/www/html/index.html
+                EOF
+  tags = {
+    Name = "web-server"
+  }              
+}
