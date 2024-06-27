4817767a3e5a32ddf5d68b388479400d210c84e7
Sahil Randhawa <98905306+sahil-randhawa@users.noreply.github.com>
2024-06-26 21:40:07 -0400

    update till step 6 - security group

--- aws/practice-project/main.tf
+++ aws/practice-project/main.tf
@@ -13,4 +13,121 @@ provider "aws" {
   region  = "us-west-2"
 }
 
-# # 1. Create vpc
+# 1. Create vpc
+
+resource "aws_vpc" "prod-vpc" {
+  cidr_block = "10.0.0.0/16"
+  tags = {
+    Name = "production"
+  }
+}
+
+# 2. Create Internet Gateway
+
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.prod-vpc.id
+
+  tags = {
+    Name = "prod-int-gw"
+  }
+}
+
+# 3. Create Custom Route Table
+
+resource "aws_route_table" "prod-route-table" {
+  vpc_id = aws_vpc.prod-vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
+
+  route {
+    ipv6_cidr_block        = "::/0"
+    egress_only_gateway_id = aws_internet_gateway.gw.id
+  }
+
+  tags = {
+    Name = "Prod"
+  }
+}
+
+# # 4. Create a Subnet 
+
+resource "aws_subnet" "subnet-1" {
+  vpc_id = aws_vpc.prod-vpc.id
+  cidr_block = "10.0.1.0/24"
+
+  tags = {
+    Name = "prod-subnet"
+  }
+}
+
+# # 5. Associate subnet with Route Table
+
+resource "aws_route_table_association" "a" {
+  subnet_id      = aws_subnet.subnet-1.id
+  route_table_id = aws_route_table.prod-route-table.id
+}
+
+# # 6. Create Security Group to allow port 22,80,443
+
+resource "aws_security_group" "allow_web" {
+  name        = "allow_web_traffic"
+  description = "Allow Web inbound traffic"
+  vpc_id      = aws_vpc.prod-vpc.id
+
+  tags = {
+    Name = "allow_web_traffic"
+  }
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_https_ipv4" {
+  security_group_id = aws_security_group.allow_web.id
+  cidr_ipv4         = [0.0.0.0/0]
+  from_port         = 443
+  ip_protocol       = "tcp"
+  to_port           = 443
+}
+
+#resource "aws_vpc_security_group_ingress_rule" "allow_https_ipv6" {
+#  security_group_id = aws_security_group.allow_web.id
+#  cidr_ipv6         = aws_vpc.prod-vpc.ipv6_cidr_block
+#  from_port         = 443
+#  ip_protocol       = "tcp"
+#  to_port           = 443
+#}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_http_ipv4" {
+  security_group_id = aws_security_group.allow_web.id
+  cidr_ipv4         = [0.0.0.0/0]
+  from_port         = 80
+  ip_protocol       = "tcp"
+  to_port           = 80
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_ssh_ipv4" {
+  security_group_id = aws_security_group.allow_web.id
+  cidr_ipv4         = [0.0.0.0/0]
+  from_port         = 22
+  ip_protocol       = "tcp"
+  to_port           = 22
+}
+
+resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
+  security_group_id = aws_security_group.allow_web.id
+  cidr_ipv4         = "0.0.0.0/0"
+  ip_protocol       = "-1" # semantically equivalent to all ports
+}
+
+resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv6" {
+  security_group_id = aws_security_group.allow_tls.id
+  cidr_ipv6         = "::/0"
+  ip_protocol       = "-1" # semantically equivalent to all ports
+}
+
+# # 7. Create a network interface with an ip in the subnet that was created in step 4
+
+# # 8. Assign an elastic IP to the network interface created in step 7
+
+# # 9. Create Ubuntu server and install/enable apache2
